b2f5d5e226444a2ce45c6d8c13cc32999a629bd5
dennis wang <dwang0816@gmail.com>
2019-06-10 23:28:18 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,3 +20,11 @@ def assign_rooms(name)
   rooms
 end
 
+def printer(name)
+  batch_badge_creator(name).each do |badges|
+    puts badges
+  end
+  assign_rooms(name).each do |room_assignment|
+    puts room_assignment
+  end
+end
